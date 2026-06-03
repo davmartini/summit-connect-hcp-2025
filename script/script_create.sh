c348e10db80c19c30226d4425d8a65089fba7b92
David Martini <dmartini@redhat.com>
2026-06-03 11:41:12 +0000

    update demo

--- script/script_create.sh
+++ script/script_create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in $(seq -w 1 10); do
+for i in $(seq -w 2 5); do
 echo hcp$i
-cat pull.tmp | sed "s/-tmp/$i/g" > pull.hcp$i
-cat key.tmp | sed "s/-tmp/$i/g" > key.hcp$i
+cat pull.tmp | sed "s/-tmp/-hcp0$i/g" > pull.hcp$i
+cat key.tmp | sed "s/-tmp/-hcp0$i/g" > key.hcp$i
 oc apply -f pull.hcp$i
 oc apply -f key.hcp$i
 rm -rf pull.hcp$i key.hcp$i
